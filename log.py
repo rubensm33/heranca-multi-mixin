a658ab44de416eefb084bd627a087d89ff48afad
rubensm33 <113398437+rubensm33@users.noreply.github.com>
2022-12-23 13:05:42 -0300

    Add files via upload

---
+++ log.py
@@ -0,0 +1,42 @@
+#Abstração
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+class Log:
+    def _log(self, msg):
+        raise NotImplementedError('Implemente o método log')
+    
+
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+
+
+class LogFileMixin(Log):
+    def _log(self, msg):
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+
+        print('Salvando no Log...', msg_formatada)
+
+        with open(LOG_FILE, 'a', encoding='utf8') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+   
+
+
+if __name__ == '__main__':
+    lp = LogPrintMixin()
+    lp.log_error('Falha')
+    lp.log_success('Sucesso')
+    lf = LogFileMixin()
+    lf.log_error('Falha')
+    lf.log_success('Sucesso')
+    
